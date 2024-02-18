6ff402d02f90092e1c4b6fe2a693b09e7f055522
Andrii Priadko <andrii.priadko@tentens.tech>
2024-02-18 22:25:26 +0200

    fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import store from '../store'
 
-const routes = [
+const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'MainLayout',
